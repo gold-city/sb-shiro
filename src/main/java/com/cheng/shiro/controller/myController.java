38587846b209c32150b1258e34ced73fbc1ce81f
city <484155842@qq.com>
2020-02-25 21:21:23 +0800

    spring boot - shrio

--- src/main/java/com/cheng/shiro/controller/myController.java
+++ src/main/java/com/cheng/shiro/controller/myController.java
@@ -2,23 +2,23 @@ package com.cheng.shiro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/testController")
 public class myController {
 
-    @ResponseBody
-    @RequestMapping(value = "/test")
-    public String test(){
-        return "test";
+    @RequestMapping(value = "/add")
+    public String add(){
+        return "/user/add.html";
     }
 
-    @RequestMapping(value = "/hello")
-    public String test1(Map<String,Object> map){
-        map.put("hello","hello");
-        return "index";
+    @RequestMapping(value = "/update")
+    public String update(){
+        return "/user/update";
+    }
+
+    @RequestMapping(value = "/toLogin")
+    public String toLogin(){
+        return "/user/login";
     }
 }
--- src/main/java/com/cheng/shiro/shiro/ShiroConfig.java
+++ src/main/java/com/cheng/shiro/shiro/ShiroConfig.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 //shiro权限框架配置类
 @Configuration//申明改类为spring配置类
 public class ShiroConfig {
@@ -22,10 +25,35 @@ public class ShiroConfig {
     /*
     * 创建ShrioFilterFactoryBean
     * */
+    @Bean
     public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("getdDefaultWebSecurityManager") DefaultWebSecurityManager defaultWebSecurityManager){
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         //设置安全管理器
         shiroFilterFactoryBean.setSecurityManager(defaultWebSecurityManager);
+
+        //添加shrio过滤器
+        /*
+        * Shiro内置过滤器，可以实现权限相关的拦截器
+        *   常用过滤器：
+        *       anon:无需认证（登录）可以访问
+        *       authc:必须认证才可以访问
+        *       user:如果使用rememberMe的功能可以直接访问
+        *       perms:该资源必须得到资源权限才可以使用
+        *       role:该资源必须得到角色权限才可以访问
+        * */
+
+        Map<String,String> filterMap=new LinkedHashMap<String, String>();
+        /*filterMap.put("/testController/add","authc");//使用authc拦截/add请求，进行权限认证
+        filterMap.put("/testController/update","authc");*/
+        //使用通配符设定目录下拦截
+        filterMap.put("/testController/*","authc");//拦截testcontroller接口下的全部接口
+        //处理testcontroller下不需要拦截的接口
+        filterMap.put("/testController/toLogin","anon");
+
+        //设置跳转的登录页面
+        shiroFilterFactoryBean.setLoginUrl("/testController/toLogin");
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterMap);
         return shiroFilterFactoryBean;
     }
 
